c11bc539bf085b40e9605a356f8f3fd2c38d2b5a
Cecilie <cjacob20@student.aau.dk>
2025-03-06 11:01:47 +0100

    Setting up basic structure
    Setup of router and super basic structure with landing page, task1, buttons and input components.

---
+++ my-react-app/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+const Landing = () => {
+    return (
+        <div>
+            <h1>Landing page</h1>
+            <button>Start</button>
+        </div>
+    )
+}
+
+export default Landing;
---
+++ my-react-app/src/pages/Task1.jsx
@@ -0,0 +1,9 @@
+const Task1 = () => {
+    return (
+        <div>
+            <h1>Task 1</h1>
+        </div>
+    )
+}
+
+export default Task1;
