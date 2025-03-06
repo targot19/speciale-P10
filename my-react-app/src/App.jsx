2f8fe7f6c0b87cef6fe00c126156f7c1be49689e
targot19 <targot20@student.aau.dk>
2025-03-06 14:37:11 +0100

    Made API folder and a file named openai.jsx  Co-authored-by: Cecilie <cecilieej@users.noreply.github.com> Co-authored-by: Emma <EmmaHoltegaard@users.noreply.github.com>

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import Task1 from "./pages/Task1";
 import Task2 from "./pages/Task2";
 import Task3 from "./pages/Task3";
 import Task4 from "./pages/Task4";
-import './App.css'
+//import './App.css'
 
 function App() {
 
