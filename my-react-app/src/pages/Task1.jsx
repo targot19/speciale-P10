dd27e3aa93343fdde868a1c5d055f0e0e2d1007d
targot19 <targot20@student.aau.dk>
2025-03-06 13:21:19 +0100

    Fixed NextButton so it works!

--- my-react-app/src/pages/Task1.jsx
+++ my-react-app/src/pages/Task1.jsx
@@ -10,30 +10,3 @@ const Task1 = () => {
 }
 
 export default Task1;
-
-const MoreButton = styled.button`
-    // background: #2d2b2b;
-    // color: #E4EDED;
-    color: #2d2b2b;
-    border: #2d2b2b 2px solid;
-    //background: #BDB153;
-    background: #E4EDED;
-    border-radius: 10px;
-    font-size: 1.2rem;
-    padding: 15px 25px;
-    font-weight: bold;
-    cursor: pointer;
-    //border: none;
-    transition: border 0.2s ease-in-out;
-    font-family: "Poppins", serif;
-    display: flex;
-    align-items: center;
-    gap: 12px;
-
-
-    &:hover {
-        //border-right: 3px solid #2d2b2b;
-        //border-bottom: 4px solid #2d2b2b;
-        border: 3px solid #2d2b2b;
-    }
-`
