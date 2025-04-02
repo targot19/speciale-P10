dc99c02ec147c662c34e8ba0b0bc30e4a6f23f2c
targot19 <targot20@student.aau.dk>
2025-04-02 13:28:46 +0200

    fixed perceivedtrust

--- my-react-app/src/components/PerceivedTrust.jsx
+++ my-react-app/src/components/PerceivedTrust.jsx
@@ -45,29 +45,29 @@ const PerceivedTrust = () => {
     return (
         <form onSubmit={handleSubmit} className="pt-0">
         <div className="flex items-center justify-center">
-            <div className="flex flex-col gap-6 w-full max-w-3xl items-center">
-            <div className="bg-gray-200 p-4 mb-4 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
+            <div className="flex flex-col gap-7 w-full max-w-3xl items-center">
+            <div className="bg-gray-200 p-2 mb-3 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
                 <ConditionQuestionBox
                     question="I believe that the chatbot would act in my best interest."
                     options={options}
                     onChange={(answer) => handleResponses("bestinterest", answer)}
                 />
                 </div>
-                <div className="bg-gray-200 p-4 mb-4 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
+                <div className="bg-gray-200 p-2 mb-3 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
                 <ConditionQuestionBox
                     question="I would characterize the chatbot as honest."
                     options={options}
                     onChange={(answer) => handleResponses("honest", answer)}
                 />
                 </div>
-                <div className="bg-gray-200 p-4 mb-4 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
+                <div className="bg-gray-200 p-2 mb-3 rounded-lg shadow-md w-full" style={{ maxWidth: "600px" }}>
                 <ConditionQuestionBox
                     question="The chatbot is competent and effective in providing answers."
                     options={options}
                     onChange={(answer) => handleResponses("effectivecompetent", answer)}
                     />
                 </div>
-                    <NextButton type="submit">
+                    <NextButton type="submit" className="flex justify-end">
                         Next
                     </NextButton>
                 </div>
