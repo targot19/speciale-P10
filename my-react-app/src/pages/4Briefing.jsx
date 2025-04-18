d71991e1280f42a03f4cfded24c7fc6909a5e321
Cecilie <cjacob20@student.aau.dk>
2025-04-18 16:19:09 +0200

    Pssst!

--- my-react-app/src/pages/4Briefing.jsx
+++ my-react-app/src/pages/4Briefing.jsx
@@ -11,7 +11,7 @@ const Briefing = () => {
 
                         You will have to answer <b>five questions</b> within <b>four different categories</b>,
                         which means you will have to answer <b>20 questions</b> in total.
-                        With every question, you can make the chatbot assist you in finding the answer.
+                        With every question, you can make the chatbot assist you in finding the answer. Psst... You can click the question box and transfer it to the input field! 
                             Additionally, you have the opportunity to view a top answer from Google for each question <i>after</i> you've prompted the chatbot <u>at least once.</u><br /><br />
 
                         Every question has a correct yes-or-no answer.
